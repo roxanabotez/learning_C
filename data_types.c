2a039cff152fa9c0ba60a8b3569ccc08c1e5289d
roxanabotez <andronache_roxana@yahoo.com>
2016-11-04 20:48:17 +0200

    Add files via upload

---
+++ data_types.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+int a1;
+printf("Introduce an integer value:  ");
+scanf ("%d",&a1);
+printf("The length of integer is:", sizeof(int));
+printf("Value is:%d\n",a1);
+return 0;
+}
